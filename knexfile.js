8cc24b937fa55fe3e0f0c552f6f6549e2bc2ac4e
mariekerkstoel <mariekerkstoel@gmail.com>
2018-11-06 18:27:01 +0000

    Add db folder, knexfile

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const BASE_PATH = path.join(__dirname, "src/db");
+const DATABASE = process.env.SAVVY_DATABASE_NAME;
+
+console.log("In migrations file");
+console.log(BASE_PATH);
+
+module.exports = {
+  test: {
+    client: "pg",
+    connection: `postgres://${process.env.LOCAL_DB_USERNAME}:${
+      process.env.LOCAL_DB_PASSWORD
+    }@localhost:5432/${DATABASE}_test`,
+    migrations: {
+      directory: path.join(BASE_PATH, "migrations", "V0.5.0")
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, "seeds/test")
+    }
+  },
+  development: {
+    client: "pg",
+    connection: `postgres://${process.env.LOCAL_DB_USERNAME}:${
+      process.env.LOCAL_DB_PASSWORD
+    }@localhost:5432/${DATABASE}`,
+    migrations: {
+      directory: path.join(BASE_PATH, "migrations", "V0.5.0")
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, "seeds/dev")
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(BASE_PATH, "migrations", "V0.5.0")
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, "seeds/dev")
+    }
+  }
+};
